8759eb2607407328cb8e7986d1e93f0b655f139c
sushmullur <mullur.sushruth@gmail.com>
2024-02-18 16:12:22 +0530

    Cat skeleton code

--- src/cat.rs
+++ src/cat.rs
@@ -1,3 +1,4 @@
-pub fn print() {
-    println!("Called cat::print()");
+pub fn process_command(args: core::str::SplitWhitespace) {
+    let args: Vec<&str> = args.collect();
+    println!("cat command called with args: {:?}", args);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
+use std::io::{self, Write};
 mod cat;
 
 fn main() {
-    println!("Hello, world!");
-    cat::print();
+    loop {
+        print!("osh> "); 
+        io::stdout().flush().unwrap(); // Flush to make sure osh> is printed before input.
+
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap(); // Read input.
+        let command = line.trim(); // Remove trailing newline.
+        if command == "exit" {
+            break;
+        }
+        process_command(command);
+    }
+}
+
+fn process_command(command: &str) {
+    let mut parts = command.split_whitespace();
+    let command = parts.next().unwrap();
+    let args = parts;
+    if command == "cat" {
+        cat::process_command(args);
+    }
 }
