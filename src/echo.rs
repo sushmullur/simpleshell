1caf4331c472db78d429e23e022751b204b31aac
sushmullur <mullur.sushruth@gmail.com>
2024-02-19 16:40:54 +0530

    Echo implemented

--- src/echo.rs
+++ src/echo.rs
@@ -1,5 +1,31 @@
 pub fn process_command(args: core::str::SplitWhitespace) {
-    // Convert args to a Vec<&str> so we can access by index.
     let args: Vec<&str> = args.collect();
-    println!("{}", args.join(" "));
+
+    let mut print_newline = true;
+    let mut interpret_escapes = false;
+
+    let mut processed_args = Vec::new();
+
+    for arg in &args {
+        match *arg {
+            "-n" => print_newline = false,
+            "-e" => interpret_escapes = true,
+            "-E" => interpret_escapes = false,
+            _ => processed_args.push(*arg),
+        }
+    }
+
+    let mut output = processed_args.join(" ");
+    if interpret_escapes {
+        output = output.replace("\\n", "\n")
+                       .replace("\\t", "\t")
+                       .replace("\\\\", "\\")
+                       ;
+    }
+
+    print!("{}", output);
+
+    if print_newline {
+        println!();
+    }
 }
